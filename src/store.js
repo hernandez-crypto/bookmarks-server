ebfe4b225743ba8038a25cc9db461a10e9fb814e
julio-crypt0 <hernandezjulio58@ymail.com>
2019-11-01 08:50:19 -0700

    init

---
+++ src/store.js
@@ -0,0 +1,10 @@
+const bookmarks = [
+  {
+    id: '59f0f46d-cc91-4d63-a7bd-ddc39709efeb',
+    title: 'ggggggg',
+    url: 'www.facebook.com',
+    description: 'llllll',
+    rating: 1,
+  },
+];
+module.exports = { bookmarks };
